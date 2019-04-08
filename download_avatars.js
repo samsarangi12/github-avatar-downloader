00c3c588eec62ead034735bd8552b4d8ac0947d8
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-08 23:59:30 +0000

    code for downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var githubToken = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!');
@@ -16,17 +17,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     var data = JSON.parse(body);
     var outputData = [];
+    var outputDataUser = [];
     for (var i = 0; i < data.length; i++) {
       outputData.push(data[i].avatar_url)
+      outputDataUser.push(data[i].login)
     }
-    cb(err, outputData)
+   cb(err, outputData, outputDataUser)
   })
+
+}
+
+function downloadImageByURL(url, filePath) {
+  for (var i = 0; i < url.length; i++) {
+    request.get(url[i])
+           .on('error', function(err) {
+              throw err
+            })
+           .on('response', function(response) {
+              console.log("Download status: ", response.statusCode)
+            })
+           .pipe(fs.createWriteStream('./avatars/' + filePath[i] + '.jpg'));
+  }
+
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result, user) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  downloadImageByURL(result, user);
 });
 
 
