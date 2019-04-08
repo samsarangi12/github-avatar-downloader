c2e5126cd8c9f31b2869876bfbe06974f57e9c2e
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-08 22:15:06 +0000

    Commit to add the secrets file to remove the authorization token

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,23 @@
-var request = ('request');
+var request = require('request');
+var githubToken = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + githubToken.GITHUB_TOKEN
+    }
+  }
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  })
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
